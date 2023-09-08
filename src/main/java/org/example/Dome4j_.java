8c2bf9977a71b11acc3797483baad81e83ba2629
sns-chaijinrui <chai0304@163.com>
2023-09-08 15:22:16 +0800

    测试xml

--- src/main/java/org/example/Dome4j_.java
+++ src/main/java/org/example/Dome4j_.java
@@ -1,10 +1,21 @@
 package org.example;
 
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
 import org.junit.Test;
+
+import java.io.File;
+
+//import org.dom4j.io.SAXReader;
 public class Dome4j_ {
-//    如何加载xml文件
+    //    如何加载xml文件
     @Test
-    public static void loadXml() {
+    public  void loadXml() throws DocumentException {
+//        得到一个解析器
+        SAXReader saxReader = new SAXReader();
+        Document document = saxReader.read(new File("C:\\Users\\chai0\\IdeaProjects\\hspedu_xml\\src\\main\\java\\org\\example\\students.xml"));
+        System.out.println(document);
 
     }
 }
