65f59f9ed52b2ea72a32357217fa329fc7858e77
sns-chaijinrui <chai0304@163.com>
2023-09-08 11:53:12 +0800

    测试xml

--- src/main/java/org/example/Dome4j_.java
+++ src/main/java/org/example/Dome4j_.java
@@ -1,5 +1,16 @@
- public class Dome4j_{
+package org.example;
 
- }
+import org.junit.Test;
+public class Dome4j_ {
+//    如何加载xml文件
+    @Test
+    public static void loadXml() {
+
+    }
+}
+
+
+
+
 
 
